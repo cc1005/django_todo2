756fab17e2f56cd3839945c91d8ef532815f5644
Conor Campbell <conorkampbell@hotmail.com>
2020-04-16 11:57:44 +0000

    Begun testing

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -5,3 +5,9 @@ class TestToDoItemForm(TestCase):
 
     def test_can_create_an_item_without_just_a_name(self):
         form = ItemForm({'name': 'Create Tests'})
+        self.assertTrue(form.is_valid())
+
+    def test_correct_message_for_missing_name(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [u'This field is required.'])
